0e2ef0f09a3293b4a7223e24839fb6aa073d7ff4
melissaair <100801990+melissaair@users.noreply.github.com>
2022-07-10 17:09:20 -0700

    Updated titles to make SEO friendly

--- script.js
+++
@@ -1,9 +0,0 @@
-var height = $('navigation').height();
-
-$(window).scroll(function () {
-    if($().scrollTop() > height) {
-        $('.navbar').addClass('fixed');
-    }else{
-        $('.navbar').removeClass('fixed');
-    }
-});
